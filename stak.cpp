34f485c100f5a4a07258ca9adcee1655667eef95
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-05 11:11:45 +0700

    case 2

--- stak.cpp
+++ stak.cpp
@@ -88,6 +88,12 @@ int main (){
             cin >> value;
             stack.push (value);
             break;
+
+            case 2:
+            stack.pop();
+            break;
         }
+       
+
     }
 }
