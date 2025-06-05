48f3c26c82028d0321fdaa61791de45a930b6d55
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-05 10:40:57 +0700

    menambahkah bool empty

--- stak.cpp
+++ stak.cpp
@@ -10,3 +10,27 @@ class node{
         next = NULL;
     }
 };
+
+class stak
+{
+    private:
+    node*top;
+
+    public:
+    stak(){
+        top = NULL;
+    }
+    int push(int value){
+        node * newnode =new node();
+        newnode->data = value;
+        newnode->next = top;
+        top = newnode;
+        cout << "push value: " << value << endl;
+        
+        return value;
+    }
+
+    bool isEmpty(){
+        return top ==NULL;
+    } 
+};
